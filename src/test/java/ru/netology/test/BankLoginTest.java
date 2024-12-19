06efee0af3abc09f010521e16438757d344bc2cb
Nikolay <masknik1666@yandex.ru>
2024-12-19 16:06:15 +0300

    init project

---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,51 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown(){
+        cleanAuthCodes();
+    }
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp(){
+        loginPage = open("http://localhost:9999" , LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login from sut test data")
+    void shouldSuccessfulLogin(){
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.login(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login whith exist in base and active user and random verification code")
+    void shouldBeErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.login(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан код! Попробуйте ещё раз.");
+    }
+
+
+}
